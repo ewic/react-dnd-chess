92cb53c03684ef19026d244d963062bd858fcf82
Eric Zhang <ewicexclamationpoint@gmail.com>
2021-07-08 01:19:13 -0400

    Init Commit

---
+++ src/Square.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Square(black, children) {
+	const fill = black ? 'black' : 'white';
+	const stroke = black ? 'white' : 'black';
+
+	return (
+		<div
+			style={{
+				backgroundColor: fill,
+				height: '100%',
+				width: '100%',
+			}}>
+			{children}
+		</div>
+	)
+}
